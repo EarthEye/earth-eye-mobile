8a71b56dcf1f917c9ed5643ca42c958bba7fee11
Saja Fakhraldeen <saja.fakhraldeen@gmail.com>
2019-01-10 15:02:14 +0300

    created footer with two buttons fwith rerouting functionality

--- earth-eye-app/Components/Measure/index.js
+++ earth-eye-app/Components/Measure/index.js
@@ -3,12 +3,20 @@ import React, { Component } from "react";
 // NativeBase Components
 import { Container, Header, Text } from "native-base";
 
+// Components
+import AppFooter from "../AppFooter";
+
 // Style
 import styles from "./styles";
 
 class Measure extends Component {
   render() {
-    return <Text>Measure Page</Text>;
+    return (
+      <Container>
+        <Text>Measure Page</Text>
+        <AppFooter />
+      </Container>
+    );
   }
 }
 
