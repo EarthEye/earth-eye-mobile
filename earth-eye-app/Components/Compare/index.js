73343e4d3f050683dbe87d7f6b63e6bf1413d3c8
Saja Fakhraldeen <saja.fakhraldeen@gmail.com>
2019-01-10 14:05:22 +0300

    landing page created with active rerouting buttons

---
+++ earth-eye-app/Components/Compare/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+// NativeBase Components
+import { Container, Header, Text } from "native-base";
+
+// Style
+import styles from "./styles";
+
+class Compare extends Component {
+  render() {
+    return <Text>Compare Page</Text>;
+  }
+}
+
+export default Compare;
---
+++ earth-eye-app/Components/Measure/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+// NativeBase Components
+import { Container, Header, Text } from "native-base";
+
+// Style
+import styles from "./styles";
+
+class Measure extends Component {
+  render() {
+    return <Text>Measure Page</Text>;
+  }
+}
+
+export default Measure;
